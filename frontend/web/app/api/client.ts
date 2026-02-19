caf2b2cef4efd72ae996c3e1c5624eb7d6e984d6
Felipe Scalco <fscalco7@gmail.com>
2026-02-19 14:19:37 -0300

    fix [WEB]: API URL

--- frontend/web/app/api/client.ts
+++ frontend/web/app/api/client.ts
@@ -3,7 +3,7 @@ import { useToastStore } from "~/stores/toast";
 import type { IStandardError } from "~/utils/interfaces/standardError";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
