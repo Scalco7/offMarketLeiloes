d74dbd5f546319f3a8b79c23473851254dc28603
Felipe Scalco <fscalco7@gmail.com>
2026-02-19 14:29:55 -0300

    fix [API]: allow cors for api

--- backend/src/main/java/com/backend/offMarketLeiloes/infrastructure/security/SecurityConfiguration.java
+++ backend/src/main/java/com/backend/offMarketLeiloes/infrastructure/security/SecurityConfiguration.java
@@ -51,7 +51,8 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration
-                .setAllowedOrigins(List.of("http://localhost:3000", "https://frontend-production-6ccc.up.railway.app"));
+                .setAllowedOrigins(List.of("http://localhost:3000", "https://frontend-production-6ccc.up.railway.app",
+                        "https://backend-production-2598.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
